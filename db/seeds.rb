0c03a0a21e5fceb645c5f8c791d6c84298c0b428
RoRElessar <palantiri@ukr.net>
2015-07-30 22:28:27 +0300

    Added default user, now with spaces and a new line in the end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 ActiveRecord::Base.establish_connection
 ActiveRecord::Base.connection.tables.each do |table|
-next if table == 'schema_migrations'
-ActiveRecord::Base.connection.execute("TRUNCATE #{table}")
+  next if table == 'schema_migrations'
+  ActiveRecord::Base.connection.execute("TRUNCATE #{table}")
 end
 
 user = User.find_by_email('default_user@test.com')
 unless user
-user = User.new(full_name: 'Default User', email: 'default_user@test.com', password: 'password123')
-user.save!
+  user = User.new(full_name: 'Default User', email: 'default_user@test.com', password: 'password123')
+  user.save!
 end
