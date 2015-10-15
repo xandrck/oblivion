1d74935438b7e8c0e03dcbf9988d408742dd76f3
Olexandr Pryadka <xandrck@gmail.com>
2015-10-15 23:27:12 +0300

    T-30 add ability to create bookmarks_group with uploading of bookmarks, fix bookmarks_group and bookmarks views, add links to bookmarks T-30 use sass instead of css file

--- app/controllers/bookmarks_groups_controller.rb
+++ app/controllers/bookmarks_groups_controller.rb
@@ -10,9 +10,15 @@ class BookmarksGroupsController < ApplicationController
   end
 
   def create
-    @bookmarks_group = current_user.bookmarks_groups.build(bookmarks_group_params)
+    file = params[:bookmarks_group][:file] # save file to variable
+    params[:bookmarks_group].delete(:file) # remove unpermitted params
 
+    @bookmarks_group = current_user.bookmarks_groups.build(bookmarks_group_params)
     if @bookmarks_group.save
+      document = Nokogiri::HTML(file)
+      document.css('dt > a').each do |a|
+        @bookmarks_group.bookmarks.create(name: a.text, href: a['href'])
+      end
       flash[:notice] = 'Bookmarks group was successfully created.'
       redirect_to bookmarks_groups_path
     else
