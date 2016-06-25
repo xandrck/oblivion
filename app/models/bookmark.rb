1effaea4959986ea1ef6a5555e4032cc7c032d7e
Ser Voloshin <sergey.voloshin.brain@gmail.com>
2016-06-25 12:34:23 +0300

    Change hash syntax.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ActiveRecord::Base
   belongs_to :bookmarks_group
 
   validates :name, :bookmarks_group_id, presence: true
-  validates :href, :format => URI::regexp(%w(http https)), presence: true
+  validates :href, format: URI::regexp(%w(http https)), presence: true
 end
